a9a1cba27d850e8e017ee0afdb4a0ee31cb046c6
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2020-01-22 15:55:44 +0000

    Start work on making code better to read

--- __init__.py
+++ __init__.py
@@ -1,12 +1,17 @@
 # RPG Player
 class Player():
+
     # RPG Player class.
     # Has a name, class and profession
+
     def __init__(self, name, p_class, p_profession):
+
         if not type(p_class) is PlayerClass:
             raise TypeError('py-rpg: p_class must be a PlayerClass type!')
+
         if not type(p_profession) is PlayerProfession:
             raise TypeError('py-rpg: profession must be a PlayerProfession type!')
+
         self.name = name
         self.max_hp = p_class.base_hp
         self.hp = p_class.base_hp
@@ -18,23 +23,32 @@ class Player():
         self.speed = p_class.base_spd
 
     def create_changes_dict(self, delta_hp, delta_st, delta_atk, delta_def, delta_spd):
-        # Used for making a changes object for a player's update function
+
+        # Used for making a dictionary for a player's update function
+
         output = {}
         if not delta_hp == None:
             output["hp"] = delta_hp
+
         if not delta_st == None:
             output["st"] = delta_st
+
         if not delta_atk == None:
             output["atk"] = delta_atk
+
         if not delta_def == None:
             output["def"] = delta_def
+
         if not delta_spd == None:
             output["spd"] = delta_spd
+
         return output
 
     def update(self, changes):
-        if isinstance(changes, dict): # Check if 'changes' is a dictionary.
+
+        if type(changes) is dict: # Check if 'changes' is a dictionary.
             raise TypeError('\'changes\' should be a dictionary!')
+
         for key, value in changes.items():
             if key == "health":
                 self.hp += value
@@ -48,30 +62,39 @@ class Player():
 
 # Player Class
 class PlayerClass():
+
     def __init__(self, name, health, stamina, attack, defense, speed):
+
         if not type(name) is str:
             raise TypeError('py-rpg: name must be an integer!')
+
         self.name = name
         
         if not type(health) is int:
             raise Exception('py-rpg: health must be an integer!')
+
         self.base_hp = health
 
         if not type(stamina) is int:
             raise TypeError('py-rpg: stamina must be an integer!')
+
         self.base_st = stamina
 
         if not type(attack) is int:
             raise TypeError('py-rpg: attack must be an integer!')
+
         self.base_atk = attack
 
         if not type(defense) is int:
             raise TypeError('py-rpg: defense must be an integer!')
+
         self.base_def = defense
 
         if not type(speed) is int:
             raise TypeError('py-rpg: speed must be an integer!')
+
         self.base_spd = speed
+
 
     def info(self):
         result = "Class '" + self.name + "' info\nBase Stats:\n  Health: "+str(self.base_hp)+"\n  Stamina: "
@@ -81,27 +104,30 @@ class PlayerClass():
 
 # Profession
 class PlayerProfession():
+
     def __init__(self, name, inventory):
+        if not type(name) is str:
+            raise TypeError('py-rpg: name must be a string!')
+
         self.name = name
-        if not type(inventory) is Inventory:
-            raise TypeError('py-rpg: inventory must be a dictionary!')
-        for i in inventory:
-            if not type(i) is Item:
-                raise TypeError('py-rpg: starter_inv must contain an Item objects!')
+        if not type(inventory) is Inventory: # Check if inventory is an Inventory object
+            raise TypeError('py-rpg: inventory must be an Inventory object!')
+        
         self.starter_inv = inventory
 
     def info(self):
         result = "Profession "+ self.name +" info\nStarter inventory:\n"
-        # yes pylint i know these variables do not exist, take a break for once.
-        result += "  Helmet: "+ self.starter_inv[helmet].name +"\n    Defense: "
-        result += str(self.starter_inv[helmet].defense) +"\n    Speed: "
-        result += str(self.starter_inv[helmet].speed) +"\n  Chestplate: "
-        result += self.starter_inv[chestplate].name +"\n    Defense:"
+        result += "  Helmet: "+ self.starter_inv.helmet.name +"\n    Defense: "
+        result += str(self.starter_inv.helmet.defense) +"\n    Speed: "
+        result += str(self.starter_inv.helmet.speed) +"\n  Chestplate: "
+        result += self.starter_inv.chestplate.name +"\n    Defense:"
     
 
 # Inventory & Items
 class Inventory():
-    pass
+    def __init__(self, helmet, chestplate, leggings, boots, backpack, backpack_max):
+        # Add another variable above to add another inventory slot.
+        if 
 
 class Item():
     # Item base class.  Use for making your own items.
@@ -109,7 +135,7 @@ class Item():
 
 class EffectItem(Item):
     # An item that has an affect.
-    # Create an EffectItemAttributes to create attributes for this item.
+    # Use the create_attributes function to create attributes for this item.
     def __init__(self, name, attributes):
         if not type(name) is str:
             raise TypeError('py-rpg: name must be a string!')
@@ -126,26 +152,26 @@ class EffectItem(Item):
             if not accepted:
                 raise ValueError('py-rpg: attributes must only contain recognized strings!')
         self.attr = attributes
-    def create_attributes_dict(self):
+    def create_attributes(self):
         # Helps in making an attributes dictionary
         pass
                 
 
-class WearableItem(Item):
+class Wearable(Item):
     # An item that is marked as wearable.
     # Should only be used for creating new wearable items.
     # Should only be used in tandem with a new Inventory spot.
     pass
     
-class Helmet(WearableItem):
+class Helmet(Wearable):
     # An item that is wearable as a helmet.
     pass
-class Chestplate(WearableItem):
+class Chestplate(Wearable):
     # An item that is wearable as a chestplate.
     pass
-class Leggings(WearableItem):
+class Leggings(Wearable):
     # An item that is wearable as leggings.
     pass
-class Boots(WearableItem):
+class Boots(Wearable):
     # An item that is wearable as boots.
     pass
