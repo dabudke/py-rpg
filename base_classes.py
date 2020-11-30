9f769f50d0760fde4977ede3d436b06a2f313d09
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2020-11-30 16:34:31 +0000

    Add base classes

---
+++ base_classes.py
@@ -0,0 +1,23 @@
+class Base_Character:
+    """Base class for creating characters.
+
+    Only use if you know what you are doing, otherwise use `NPC_Character` or `Player_Character`.
+    """
+    
+    pass
+
+class Base_Container:
+    """Base storage container class
+
+    Don't use if you know what you are doing, otherwise use `Inventory_Storage` or `Storage_Container`.
+    """
+
+    pass
+
+class Base_Item:
+    """Base item class
+
+    Don't use if you know what you are doing, other wise use `Item` or `Effect_Item`
+    """
+
+    pass
